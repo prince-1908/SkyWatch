02a5d66579a2e84f186f154a58e32d66478b81ed
Prince Singh Chouhan <princesinghchouhan7470@gmail.com>
2023-08-22 20:09:48 +0530

    random cities

--- javascript/otherCities.js
+++ javascript/otherCities.js
@@ -1,51 +1,126 @@
-// let cities = [
-//         "Mumbai, Maharashtra",
-//         "Delhi, Delhi",
-//         "Bengaluru, Karnataka",
-//         "Hyderabad, Telangana",
-//         "Ahmedabad, Gujarat",
-//         "Chennai, Tamil Nadu",
-//         "Kolkata, West Bengal",
-//         "Pune, Maharashtra",
-//         "Jaipur, Rajasthan",
-//         "Lucknow, Uttar Pradesh",
-//         "Kanpur, Uttar Pradesh",
-//         "Nagpur, Maharashtra",
-//         "Patna, Bihar",
-//         "Indore, Madhya Pradesh",
-//         "Thane, Maharashtra",
-//         "Bhopal, Madhya Pradesh",
-//         "Visakhapatnam, Andhra Pradesh",
-//         "Vadodara, Gujarat",
-//         "Coimbatore, Tamil Nadu",
-//         "Ludhiana, Punjab",
-//         "Kochi, Kerala",
-//         "Agra, Uttar Pradesh"
-//     ]
-    
+let cityName = document.getElementsByClassName("cityName");
+let cityCloudPct = document.getElementsByClassName("cityCloudPct");
+let cityTemp = document.getElementsByClassName("cityTemp");
+let feelsLike = document.getElementsByClassName("feelsLike");
+let cityHumidity = document.getElementsByClassName("cityHumidity");
+let cityWindDir = document.getElementsByClassName("cityWindDir");
+let cityWindSpeed = document.getElementsByClassName("cityWindSpeed");
 
-    // let randomCities = [];
+const cities = [
+    "Mumbai",
+    "Delhi",
+    "Bangalore",
+    "Hyderabad",
+    "Chennai",
+    "Kolkata",
+    "Pune",
+    "Ahmedabad",
+    "Jaipur",
+    "Chandigarh",
+    "Lucknow",
+    "Kochi",
+    "Indore",
+    "Surat",
+    "Agra",
+    "Varanasi",
+    "Bhopal",
+    "Ludhiana",
+    "Nagpur",
+    "Patna",
+    "Kanpur",
+    "Amritsar",
+    "Coimbatore",
+    "Vadodara",
+    "Madurai",
+    "Visakhapatnam",
+    "Allahabad",
+    "Ranchi",
+    "Thiruvananthapuram",
+    "Bhubaneswar",
+    "Mysore",
+    "Mangalore",
+    "Dehradun",
+    "Raipur",
+    "Guwahati",
+    "Jamshedpur",
+    "Bikaner",
+    "Amravati",
+    "Udaipur",
+    "Jodhpur",
+    "Ajmer",
+    "Patiala",
+    "Bhavnagar",
+    "Salem",
+    "Tiruchirappalli",
+    "Gwalior",
+    "Jalandhar",
+    "Bhiwandi",
+    "Saharanpur",
+    "Gorakhpur"
+];
+// console.log(cities);
 
-    // while(randomCities.length < 5){
-    //     let randomIndex = Math.floor(Math.random() * randomCities.length);
-    //     let randomValue = cities[randomIndex];
+let randomCities = [];
 
-    //     if(!randomCities.includes(randomValue)){
-    //         randomCities.push(randomValue);
-    //     }
-    // }
-    // console.log(randomCities);
+while (randomCities.length < 5) {
+    let randomIndex = Math.floor(Math.random() * cities.length);
+    let randomCity = cities[randomIndex];
+    if (!randomCities.includes(randomCity)) {
+        randomCities.push(randomCity);
+    }
+}
+console.log(randomCities);
 
-    // const random = Math.floor(Math.random() * cities.length);
-    // console.log(random, cities[random]);
+for (let i = 0; i < randomCities.length; i++) {
+    fetch(`https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${randomCities[i]}`, {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'c9d9d23d86mshf1c4bc63c9653d7p1cc8a2jsnea25016d9c0f',
+            'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+        }
+    })
+        .then(response => response.json())
+        .then((response) => {
 
+            let windDir = (wd) => {
+                if (wd >= 22.5 && wd <= 67.5) {
+                    cityWindDir[i].innerHTML = "North-East";
+                }
+            
+                else if (wd >= 67.5 && wd <= 112.5) {
+                    cityWindDir[i].innerHTML = "East";
+                }
+            
+                else if (wd >= 112.5 && wd <= 157.5) {
+                    cityWindDir[i].innerHTML = "South-East";
+                }
+            
+                else if (wd >= 157.5 && wd <= 202.5) {
+                    cityWindDir[i].innerHTML = "South";
+                }
+                else if (wd >= 202.5 && wd <= 247.5) {
+                    cityWindDir[i].innerHTML = "South-West";
+                }
+                else if (wd >= 247.5 && wd <= 292.5) {
+                    cityWindDir[i].innerHTML = "West";
+                }
+                else if (wd >= 292.5 && wd <= 337.5) {
+                    cityWindDir[i].innerHTML = "North-West";
+                }
+            
+                else if ((wd >= 337.5 && wd <= 360) || (wd >= 0 && wd <= 22.5)) {
+                    cityWindDir[i].innerHTML = "North";
+                }
+            }
 
-    // fetch(`https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${cities[random]}`, options)
-	// .then(response => response.json())
-	// .then((response) => {
-		
-	// 	console.log(response)
-
-		
-	// })
-	// .catch(err => console.error(err));
+            console.log(response)
+            cityName[i].innerHTML = randomCities[i];
+            cityCloudPct[i].innerHTML = response.cloud_pct + "%";
+            cityTemp[i].innerHTML = response.temp + "&degC";
+            feelsLike[i].innerHTML = response.feels_like + '&degC';
+            cityHumidity[i].innerHTML = response.humidity + "%";
+            windDir(response.wind_degrees);
+            cityWindSpeed[i].innerHTML = response.wind_speed + 'm/s';
+        })
+        .catch(err => console.error(err));
+}
